78aa2198d4a0a5dd49a618577da41295553ef488
Shubham Taywade <shubhamtaywade82@gmail.com>
2020-07-03 23:40:47 +0530

    ui update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
+    @branches = Branch.all
+    @branch = Branch.all
+    @subjects = Subject.all
+    @faculties = Faculty.all
   end
 end
